39c82948b154dfb42ea5148cb9d42b414251e2cb
Anita <anita@joli.com.au>
2026-03-31 12:49:19 +0800

    Moved batch code from smtp2go_client.rb into core.

--- lib/smtp2go/core.rb
+++ lib/smtp2go/core.rb
@@ -6,50 +6,90 @@ require 'smtp2go/settings'
 module Smtp2go
   # Ruby Library for interacting with the smtp2go API
   class Smtp2goClient
-    # Client to handle API interfacing
-    attr_reader :headers, :send_endpoint
-
-    def initialize(api_key: ENV['SMTP2GO_API_KEY'])
+    attr_reader :api_key, :headers, :send_endpoint, :batch_endpoint
+    def initialize (**options)
+      @api_key = options[:api_key] || ENV['SMTP2GO_API_KEY']
+      @headers = {**HEADERS, "X-Smtp2go-Api-Key" => api_key}
       @send_endpoint = SEND_ENDPOINT
       @batch_endpoint = BATCH_ENDPOINT
-      @headers = {**HEADERS, "X-Smtp2go-Api-Key" => api_key}
       raise Smtp2goAPIKeyException unless api_key
     end
 
-    # @param sender [String] the from email address
-    # @param recipients [Array <String>] the email address of the recipient(s)
-    # @param subject [String] the email subject
-    # @param text [String] the email text content (optional if html or template is passed)
-    # @param html [String] the email html content (optional if text or template is passed)
-    # @param template [hash] template{:id, :dats} (optional if text or html is passed)
-    #
-    # @return [Smtp2goResponse] response object
-    def send(sender:, recipients:, subject:, template: {}, text: nil, html: nil)
-      raise Smtp2goParameterException unless [html, text, template].any?
-      raise Smtp2goTemplateException unless (template.empty? || (template.keys - TEMPLATE_KEYS).empty?)
-      payload = {
-        sender: sender,
-        to: recipients,
-        subject: subject,
-        text_body: text,
-        html_body: html,
-        template_id: template[:id] || nil,
-        template_data: template[:data] || nil
-      }.compact
+  end
 
-      response = HTTParty.post(
-        send_endpoint,
-        { body: JSON.dump(payload),
-          headers: {**headers }
-        }
-      )
-      Smtp2goResponse.new response
-    end
+  # @param sender [String] the from email address
+  # @param recipients [Array <String>] the email address of the recipient(s)
+  # @param subject [String] the email subject
+  # @param text [String] the email text content (optional if html or template is passed)
+  # @param html [String] the email html content (optional if text or template is passed)
+  # @param template [Hash]{id:, data:} the template id and template data (optional if html or text is passed)
+  # @return [Smtp2goResponse] response object
+
+
+  def send(sender:, recipients:, subject:, template: {}, text: nil, html: nil)
+    template.compact!
+    raise Smtp2goParameterException unless [html, text, template].any?
+    raise Smtp2goTemplateException unless verify_template(template)
+
+    payload = {
+      api_key: api_key,
+      sender: sender,
+      recipients: recipients,
+      to: recipients,
+      subject: subject,
+      text_body: text,
+      html_body: html,
+      template_id: template[:id] || nil,
+      template_data: template[:data] || nil
+    }.compact
+    submit_to_smtp2go(send_endpoint, payload)
+  end
+
+  def batch(emails)
+    ap "Received #{emails.length} emails"
+    ap emails.first
+    # test_keys = %i(text html template)
+    # (valid_entries, invalid_entries) = emails.partition { |email| verify_batch_email(email.values_at(*test_keys)) }
+    #
+    # unless invalid_entries.empty?
+    #   puts "#{invalid_entries.size} invalid entries found"
+    #   raise Smtp2goParameterException
+    # end
+
+    payload = {
+      api_key: api_key,
+      emails: emails
+    }
+    submit_to_smtp2go(batch_endpoint, payload)
+  end
+
+  private
+
+  def submit_to_smtp2go(endpoint, payload)
+    response = HTTParty.post(
+      endpoint,
+      body: payload.to_json,
+      headers: headers
+    )
+    Smtp2goResponse.new response
+  end
+
+  def verify_batch_email( text: nil, html: nil, template: {})
+    ap "Verifying batch email: #{text}, html: #{html}, template: #{template}"
+    template.compact!
+    result = [html, text, template].any?
+    ap result ? "Good" : "Bad"
+    result
+  end
+
+  def verify_template(template)
+    template.empty? || (TEMPLATE_KEYS - template.keys).empty?
   end
 
   # Wraps response object with smtp2go specific data
   class Smtp2goResponse
     attr_reader :rate_limit
+
     def initialize(response)
       @response = response
       @rate_limit = RateLimit.new @response.headers
