9958ae38823c07bbde8170bfb4763a1ab2adb3b1
Anita <anita@joli.com.au>
2026-05-04 11:20:26 +0800

    Spec_helper: Add payload data for batch emails Add tests for successful and failed batches and suppressions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,12 +16,26 @@ end
 
 # Constants:
 PAYLOAD = {
-  sender: 'dave@example.com',
   recipients: ['matt@example.com'],
+  sender: 'dave@example.com',
   subject: 'smtp2go Ruby Client',
   text: 'Test message.',
   html: '<html><body><p>Test Message</p></body></html>'
 }
+
+BATCH_EMAILS =
+  [
+    { to: "recipient1@example.com",
+      sender: 'dave@example.com',
+      template_id:  "tpl-001",
+      template_data: { name: "Recipient One" }
+    },
+    { to: "recipient2@example.com",
+      sender: 'dave@example.com',
+      template_id: "tpl-001",
+      template_data: { name: "Recipient Two" }
+    }
+  ]
 
 # Helper methods:
 def get_response_object
@@ -47,5 +61,32 @@ def get_failed_response
   smtp2go_client = get_client
   VCR.use_cassette('failed_send') do
     smtp2go_client.send(**PAYLOAD)
+  end
+end
+
+def get_successful_batch
+  payload = {
+    apikey: 'testapikey',
+    emails: BATCH_EMAILS
+  }
+  VCR.use_cassette('successful_batch') do
+    smtp2go_client.batch(**payload)
+  end
+end
+
+def get_failed_batch
+  payload = {
+    apikey: 'notanapikey',
+    emails: BATCH_EMAILS
+  }
+  VCR.use_cassette('failed_batch') do
+    smtp2go_client.batch(**payload)
+  end
+end
+
+def get_suppressions
+  smtp2go_client = Smtp2go::Smtp2goClient.new(apikey: 'testapikey')
+  VCR.use_cassette('suppressions') do
+    smtp2go_client.suppressions
   end
 end
