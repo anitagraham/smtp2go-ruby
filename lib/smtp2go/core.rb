b6653389b4e0c7925373b314f9281c921b3b7d1e
Anita <anita@joli.com.au>
2026-05-04 11:17:51 +0800

    Core: add capability to use batch and suppressions endpoints
    httparty now returns response as json by default, so treat it as json

--- lib/smtp2go/core.rb
+++ lib/smtp2go/core.rb
@@ -6,31 +6,33 @@ require 'smtp2go/settings'
 module Smtp2go
   # Ruby Library for interacting with the smtp2go API
   class Smtp2goClient
-    attr_reader :api_key, :headers, :send_endpoint, :batch_endpoint
 
     def initialize (**options)
       @api_key = options[:api_key] || ENV['SMTP2GO_API_KEY']
-      @headers = { **HEADERS, "X-Smtp2go-Api-Key" => api_key }
+      raise Smtp2goAPIKeyException unless @api_key
+
+      @headers = { **HEADERS, "X-Smtp2go-Api-Key" => @api_key }
       @send_endpoint = SEND_ENDPOINT
       @batch_endpoint = BATCH_ENDPOINT
-      raise Smtp2goAPIKeyException unless api_key
+      @suppression_endpoint = SUPPRESSION_ENDPOINT
     end
 
     # @param sender [String] the from email address
     # @param recipients [Array <String>] the email address of the recipient(s)
     # @param subject [String] the email subject
     # @param text [String] the email text content (optional if html or template is passed)
-    # @param html [String] the email html content (optional if text or template is passed)
-    # @param template [Hash]{id:, data:} the template id and template data (optional if html or text is passed)
+    # @param html [String] the email HTML content (optional if text or template is passed)
+    # @param template [Hash]{id:, data:} the template id and template data (optional if HTML or text is passed)
     # @return [Smtp2goResponse] response object
-
+    # send
     def send(sender:, recipients:, subject:, template: {}, text: nil, html: nil)
-      template.compact!
+      template.compact! unless template.nil?
+
       raise Smtp2goParameterException unless [html, text, template].any?
       raise Smtp2goTemplateException unless verify_template(template)
 
       payload = {
-        api_key: api_key,
+        api_key: @api_key,
         sender: sender,
         recipients: recipients,
         to: recipients,
@@ -40,44 +42,35 @@ module Smtp2go
         template_id: template[:id] || nil,
         template_data: template[:data] || nil
       }.compact
-      submit_to_smtp2go(send_endpoint, payload)
+      submit_to_smtp2go(@send_endpoint, payload: payload)
     end
 
     def batch(emails)
-      ap "Received #{emails.length} emails"
-      ap emails.first
-      # test_keys = %i(text html template)
-      # (valid_entries, invalid_entries) = emails.partition { |email| verify_batch_email(email.values_at(*test_keys)) }
-      #
-      # unless invalid_entries.empty?
-      #   puts "#{invalid_entries.size} invalid entries found"
-      #   raise Smtp2goParameterException
-      # end
-
       payload = {
-        api_key: api_key,
+        api_key: @api_key,
         emails: emails
       }
-      submit_to_smtp2go(batch_endpoint, payload)
+      submit_to_smtp2go(@batch_endpoint, payload: payload)
+    end
+
+    def suppressions
+      submit_to_smtp2go(@suppression_endpoint, response_object: false)
     end
 
     private
 
-    def submit_to_smtp2go(endpoint, payload)
+    def submit_to_smtp2go(endpoint, payload: nil, response_object: true)
       response = HTTParty.post(
         endpoint,
-        body: payload.to_json,
-        headers: headers
-      )
-      Smtp2goResponse.new response
+        body: payload&.to_json,
+        headers: @headers
+      ).compact
+     Smtp2goResponse.new(response)
     end
 
     def verify_batch_email(text: nil, html: nil, template: {})
-      ap "Verifying batch email: #{text}, html: #{html}, template: #{template}"
       template.compact!
-      result = [html, text, template].any?
-      ap result ? "Good" : "Bad"
-      result
+      [html, text, template].any?
     end
 
     def verify_template(template)
@@ -87,27 +80,36 @@ module Smtp2go
 
   # Wraps response object with smtp2go specific data
   class Smtp2goResponse
-    attr_reader :rate_limit
+    attr_reader :rate_limit, :response
 
     def initialize(response)
       @response = response
-      @rate_limit = RateLimit.new @response.headers
+      @rate_limit = RateLimit.new response.headers if response.respond_to?(:headers)
     end
 
+    # httparty returns JSON by default
     def json
-      JSON.parse @response.body
+      @response
     end
 
     def success?
-      json['data']['succeeded'] ? true : false
+      @response['data']['succeeded'] ? true : false
     end
 
     def errors
-      json['data']['error']
+      @response['data']['error']
     end
 
     def request_id
-      json['request_id']
+      @response['request_id']
+    end
+
+    def batch_ids
+      @batch_ids ||= response['data']
+    end
+
+    def raw
+      @response
     end
 
     def status_code
@@ -120,6 +122,8 @@ module Smtp2go
     attr_reader :limit, :remaining, :reset
 
     def initialize(headers)
+      return if headers.nil?
+
       @limit = headers['x-ratelimit-limit']
       @remaining = headers['x-ratelimit-remaining']
       @reset = headers['x-ratelimit-reset']
